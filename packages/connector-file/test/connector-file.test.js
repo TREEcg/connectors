eca666811606c9a1144b10ef8f7859474a668aef
Julian Rojas <julian.rojas87@gmail.com>
2023-06-14 23:34:32 +0200

    Fix file channel tests

--- packages/connector-file/test/connector-file.test.js
+++ packages/connector-file/test/connector-file.test.js
@@ -1,11 +1,6 @@
-import { describe, test, expect } from "@jest/globals";
 import { readFile, writeFile, rename } from "fs/promises";
-import { FileStreamReaderFactory } from "..";
-import { FileStreamWriterFactory } from "../lib/StreamWriter";
-async function sleep(ms: number): Promise<unknown> {
-    return new Promise(res => setTimeout(res, ms));
-}
-
+import { describe, test, expect } from "@jest/globals";
+import { FileStreamReaderFactory, FileStreamWriterFactory } from "..";
 describe("connector-file", () => {
     test("reader", async () => {
         const factory = new FileStreamReaderFactory();
@@ -15,42 +10,33 @@ describe("connector-file", () => {
             readFirstContent: false,
             encoding: "utf-8",
         }, x => x);
-
-        const elements: string[] = [];
-
+        const elements = [];
         stream.data(x => {
             elements.push(x);
         });
         expect(stream.lastElement).not.toBeDefined();
-
         await writeFile("test.txt", "something", { encoding: "utf8" });
         await sleep(300);
-
         expect(stream.lastElement).toBeDefined();
         expect(stream.lastElement).toBe("something");
         expect(elements).toStrictEqual(["something"]);
-
         await writeFile("test.txt", "somethingelse", { encoding: "utf8" });
         await sleep(300);
         expect(stream.lastElement).toBeDefined();
         expect(stream.lastElement).toBe("somethingelse");
         expect(elements).toStrictEqual(["something", "somethingelse"]);
-
         await rename("test.txt", "test2.txt");
         await writeFile("test2.txt", "43", { encoding: "utf8" });
         await rename("test2.txt", "test.txt");
         await sleep(300);
         expect(elements).toStrictEqual(["something", "somethingelse", "43"]);
-
         await writeFile("test.txt", "foobar", { encoding: "utf8" });
         await sleep(200);
         expect(stream.lastElement).toBeDefined();
         expect(stream.lastElement).toBe("foobar");
         expect(elements).toStrictEqual(["something", "somethingelse", "43", "foobar"]);
-
         await stream.disconnect();
     });
-
     test("writer", async () => {
         const factory = new FileStreamWriterFactory();
         const writer = await factory.build({
@@ -58,21 +44,16 @@ describe("connector-file", () => {
             onReplace: true,
             readFirstContent: false,
             encoding: "utf-8",
-        }, x => <string>x);
-
+        }, x => x);
         let content = "";
-
         await writer.push("data");
-
         content = await readFile("test.txt", "utf8");
         expect(content).toBe("data");
-
         await writer.push("43");
         content = await readFile("test.txt", "utf8");
         expect(content).toBe("43");
         await writer.disconnect();
     });
-
     test("chained", async () => {
         const writerFactory = new FileStreamWriterFactory();
         const writer = await writerFactory.build({
@@ -80,7 +61,7 @@ describe("connector-file", () => {
             onReplace: true,
             readFirstContent: false,
             encoding: "utf-8",
-        }, x => <string>x);
+        }, x => x);
         const readerFactory = new FileStreamReaderFactory();
         const stream = await readerFactory.build({
             path: "test.txt",
@@ -88,17 +69,13 @@ describe("connector-file", () => {
             readFirstContent: false,
             encoding: "utf-8",
         }, x => x);
-
-        const elements: string[] = [];
+        const elements = [];
         stream.data(x => {
             elements.push(x);
         });
-
         await writer.push("data");
         await sleep(200);
-
         expect(elements).toStrictEqual(["data"]);
-
         await writer.push("43");
         await sleep(200);
         expect(elements).toStrictEqual(["data", "43"]);
@@ -106,3 +83,7 @@ describe("connector-file", () => {
         await writer.disconnect();
     });
 });
+function sleep(x) {
+    return new Promise(resolve => setTimeout(resolve, x));
+}
+//# sourceMappingURL=connector-file.test.js.map
